ba079152b72469db9b4592888f1de59f45b4665c
Ngoc Tuan <lntuancnh@gmail.com>
2025-08-08 15:57:27 +0700

    Lưu data vào database (lưu ý sử dung Prepared Statements tránh hacker tấn công

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,7 +4,7 @@ const getAllUser = async () => {
   const connection = await getConnection();
   try {
     const [results, fields] = await connection.query("SELECT * FROM `users`");
-    // console.log(results); 
+    // console.log(results);
     return results; // Lấy tất cả người dùng từ cơ sở dữ liệu
   } catch (err) {
     console.log(err);
@@ -12,13 +12,25 @@ const getAllUser = async () => {
   }
 };
 
-const handleCreateUser = (
+const handleCreateUser = async (
   fullName: string,
   email: string,
   password: string,
   address: string
 ) => {
-  console.log("insert database");
+  const connection = await getConnection();
+  try {
+    const sql =
+      "INSERT INTO `users`(`fullName`, `email`,`password`,`address`) VALUES (?, ?,?,?)";
+    const values = [fullName, email, password, address];
+
+    const [result, fields] = await connection.execute(sql, values);
+
+    console.log(result);
+    console.log(fields);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export { getAllUser, handleCreateUser };
